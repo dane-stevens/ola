46ab734190a80fc34911cabecd7ed57b527f85cb
Ola Telesz <ola.telesz@matthewscott.com>
2022-11-09 16:18:53 -0500

    more edits

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,6 +16,25 @@ export default function Index() {
     }
   ]
 
+  const orders = [
+    {
+      orderNum: 12345,
+      qty: 2,
+      item: 't-shirts',
+      size: 'xs',
+      colour: 'blue',
+      total: 30,
+    },
+    {
+      orderNum: 23456,
+      qty: 4,
+      item: 'sweaters',
+      size: 'm',
+      colour: 'black',
+      total: 120,
+    }
+  ]
+
   return (
     <div>
       <h1>Test</h1>
@@ -24,7 +43,31 @@ export default function Index() {
         people.map((person, index) => {
           return (
             <div key={index}>
-              Person: { person.firstName } {person.lastName} { person.age }
+              <b>User:</b> {index} <br/> 
+              { person.firstName } {person.lastName} <i>{ person.age } </i>
+            </div>
+          )
+        })
+      }
+      <h2>New Table</h2>
+
+      {
+        orders.map((purchase) => {
+          return (
+            <div key={purchase.orderNum}>
+              #{purchase.orderNum} {purchase.qty} {purchase.size} {purchase.colour} {purchase.item} ${purchase.total}
+              {/* <table>
+                <tr> 
+                <td> #{purchase.orderNum} </td>
+                <td> {purchase.qty} </td> 
+                <td> {purchase.size} </td>
+                <td> {purchase.colour} </td>
+                <td> {purchase.item} </td>
+                <td> ${purchase.total}</td>
+                </tr>
+              
+              </table> */}
+              
             </div>
           )
         })
